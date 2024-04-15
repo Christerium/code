a0e093a848b0ec68cc61898c08d7c98444305d0a
Christof JKU <christof.brandstetter@jku.at>
2024-04-15 11:33:02 +0200

    Created venv and implemented a short read_instance function

--- python/main.py
+++ python/main.py
@@ -1,5 +1,26 @@
+import cvxpy as cp
+import numpy as np
+
+def read_instance(file_path):
+    with open(file_path, 'r') as file:
+        pairs = []
+        costs = []
+        lengths = []
+        for line in file:
+            if line[0] == "e":
+                pairs.append((int(line.split()[1]), int(line.split()[2])))
+                costs.append(int(line.split()[3]))
+            elif line[0] == "l":
+                lengths.append(int(line.split()[1]))
+        return pairs, costs, lengths
+
 def main():
-    # Your code here
+    pairs, costs, lengths = read_instance("python/simmons_1.txt")
+
+    print(pairs)
+    print(costs)
+    print(lengths)
+
 
 if __name__ == "__main__":
     main()
