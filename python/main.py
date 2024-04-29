cdf7d24b3a2b1b699b25ec8dff0d2eb73378704e
Christof JKU <christof.brandstetter@jku.at>
2024-04-29 13:21:57 +0200

    starting with branch and bound

--- python/main.py
+++ python/main.py
@@ -17,6 +17,10 @@ import old_functions
 
 TOLERANCE = 1e-6
 
+
+
+
+
 # Define the instance structure
 class Instance(NamedTuple):
     pairs: list
@@ -144,7 +148,7 @@ def problem_mosek(instance, sum_triangle = False):
         # Diagonal constraint
         M.constraint("diag",Expr.mulDiag(X, Matrix.eye(dim)), Domain.equalsTo(1.0))
 
-        # Triangle constraints
+        # Betweenes constraints
 
         if(sum_triangle):
             for i,j in pairs:
@@ -198,7 +202,10 @@ def problem_mosek(instance, sum_triangle = False):
 
         try:
             M.solve()
+            print(X.level())
+            print(M.primalObjValue())
             
+            """
             print("Root: ")
             M.acceptedSolutionStatus(AccSolutionStatus.Optimal)
             print("Relaxation objective value: {0}".format(M.primalObjValue()))
@@ -250,7 +257,7 @@ def problem_mosek(instance, sum_triangle = False):
             print("The permutation of this solution is:", [x[1] for x in p_values])
             print("The objective value of this solution is:", permutation2objective(p_values, costs, lengths, pairs))
 
-            """
+            
             M.getConstraint(f"X_0_{element}_0").remove()
             M.constraint(f"X_0_{element}_1",X.index([0,element]), Domain.equalsTo(1.0))
 
@@ -266,10 +273,10 @@ def problem_mosek(instance, sum_triangle = False):
             p_values = solution2permutation(X.level()[0:dim], n)
             print("The permutation of this solution is:", [x[1] for x in p_values])
             print("The objective value of this solution is:", permutation2objective(p_values, costs, lengths, pairs))
-            """
+            
             # Check the rank of the matrix
             #print("Rank of X:", np.linalg.matrix_rank(X.level(),TOLERANCE))
-            
+            """
             
 
         except OptimizeError as e:
@@ -300,6 +307,155 @@ def problem_mosek(instance, sum_triangle = False):
         except Exception as e:
             print("Unexpected error: {0}".format(e))
 
+# Define the optimization problem with the triangle constraints and Z
+def problem_sdp3(instance, M, sum_betweeness = False):
+    pairs, costs, lengths = instance.pairs, instance.costs, instance.lengths    
+    costs_matrix = define_costmatrix(pairs, costs, lengths)
+    K = np.sum(costs)*np.sum(lengths)/2
+
+    n = len(lengths)
+
+    #M = Model("SDP3")
+
+    #with Model("SDP3") as M:
+
+    dim = len(pairs)
+
+    Z = M.variable("Z", Domain.inPSDCone(dim+1))
+    Y = Z.slice([1,1] , [dim+1, dim+1])
+    y = Z.slice([1,0], [1,dim+1])
+
+    C = Matrix.sparse(costs_matrix)
+
+    # Objective function
+    M.objective(ObjectiveSense.Minimize, Expr.sub(K, Expr.dot(C, Y)))
+
+    M.constraint("diag",Expr.mulDiag(Y, Matrix.eye(dim)), Domain.equalsTo(1.0))
+
+    # Betweenes constraints
+
+    if(sum_betweeness):
+        for i,j in pairs:
+            subi = []
+            subj = []
+            val = []
+            for k in range(1, n+1):
+                ij, ik, jk = get_index(i,j,n), get_index(i,k,n), get_index(j,k,n)
+                if k == i or k == j:
+                    continue
+                elif k < i:         # k < i < j -> ij * (-jk) = (-1) ||(-) ij * (-ik) = (+1) || (-) (-ik) * (-jk) = -1
+                    subi.append(ij)
+                    subj.append(jk)
+                    val.append(-1.0)
+                    subi.append(ij)
+                    subj.append(ik)
+                    val.append(1.0)
+                    subi.append(ik)
+                    subj.append(jk)
+                    val.append(-1.0)
+                elif k < j:         # i < k < j -> ij * (-jk) = (-1) || (-) ij * (ik) = (-1) || (-) (ik) * (-jk) = +1
+                    subi.append(ij)
+                    subj.append(jk)
+                    val.append(-1.0)
+                    subi.append(ij)
+                    subj.append(ik)
+                    val.append(-1.0)
+                    subi.append(ik)
+                    subj.append(jk)
+                    val.append(1.0)
+                elif k > j:               # i < j < k -> ij * jk = (+1) || (-) ij * ik = (-1) || (-) ik * jk = (-1)
+                    subi.append(ij)
+                    subj.append(jk)
+                    val.append(1.0)
+                    subi.append(ij)
+                    subj.append(ik)
+                    val.append(-1.0)
+                    subi.append(ik)
+                    subj.append(jk)
+                    val.append(-1.0)
+
+            #print(subi, subj, val)
+            A = Matrix.sparse(dim, dim, subi, subj, val)   
+
+            M.constraint(Expr.dot(A, Y), Domain.equalsTo(2-n))         
+    else:
+        for i,j in pairs:
+            for k in range(j+1, n+1):
+                ij, ik, jk = get_index(i,j,n), get_index(i,k,n), get_index(j,k,n)
+                M.constraint(Expr.sub(Expr.sub(Y.index(ij,jk), Y.index(ij,ik)), Y.index(ik,jk)), Domain.equalsTo(-1.0))
+
+    """
+    # Triangle constraints
+    test = np.array([[-1.0, -1.0, -1.0], [-1.0, 1.0, 1.0], [1.0, -1.0, 1.0], [1.0, 1.0, -1.0]])
+    triangle = Matrix.dense(test)
+
+    for i in range(1, n+1):
+        for j in range(i+1, n+1):
+            for k in range(j+1, n+1):
+                #triangle_Y = Matrix.dense(np.array([[Y.index(i,j)], [Y.index(j,k)], [Y.index(i,k)]]))
+                M.constraint(Expr.sub(Expr.mul(triangle, Expr.vstack([Y.index(i,j), Y.index(j,k), Y.index(i,k)])), Expr.ones(4)), Domain.lessThan(0.0) )
+                #pass
+    
+    try:
+        M.solve()
+        #print_solution(instance, M)
+
+    except OptimizeError as e:
+        print("Optimization failed. Error: {0}".format(e))
+
+    except SolutionError as e:
+        # The solution with at least the expected status was not available.
+        # We try to diagnoze why.
+        print("Requested solution was not available.")
+        prosta = M.getProblemStatus()
+
+        if prosta == ProblemStatus.DualInfeasible:
+            print("Dual infeasibility certificate found.")
+
+        elif prosta == ProblemStatus.PrimalInfeasible:
+            print("Primal infeasibility certificate found.")
+            
+        elif prosta == ProblemStatus.Unknown:
+        # The solutions status is unknown. The termination code
+        # indicates why the optimizer terminated prematurely.
+            print("The solution status is unknown.")
+            symname, desc = mosek.Env.getcodedesc(mosek.rescode(int(M.getSolverIntInfo("optimizeResponse"))))
+            print("   Termination code: {0} {1}".format(symname, desc))
+
+        else:
+            print("Another unexpected problem status {0} is obtained.".format(prosta))
+
+    except Exception as e:
+        print("Unexpected error: {0}".format(e))
+
+    """
+# Print the solution of the optimization problem / still too much other stuff in there
+def print_solution(instance, M):
+
+    pairs, costs, lengths = instance.pairs, instance.costs, instance.lengths
+
+    dim = len(pairs)
+    n = len(lengths)
+
+    Z = M.getVariable("Z")
+    Y = Z.slice([1,1] , [dim+1, dim+1])
+    y = Z.slice([1,0], [1,dim+1])
+            
+    print("Root: ")
+    M.acceptedSolutionStatus(AccSolutionStatus.Optimal)
+    print("Relaxation objective value: {0}".format(M.primalObjValue()))
+    #print("Rank of X:", np.linalg.matrix_rank(Y.level(),TOLERANCE))    
+
+    d = [0.0]*dim
+    mosek.LinAlg.syeig(mosek.uplo.lo, dim, Y.level(), d)
+    rank = sum([d[i] > 1e-6 for i in range(dim)])
+    #print(d)
+    print("Rank of Y:", rank)
+
+    p_values = solution2permutation(Y.level()[0:dim], n)
+    print("The permutation of this solution is:", [x[1] for x in p_values])
+    print("The objective value of this solution is:", permutation2objective(p_values, costs, lengths, pairs))
+    
 # Transform the solution of the optimization problem to a permutation
 def solution2permutation(X, n):
     R_values = []
@@ -337,11 +493,82 @@ def getLength(p_values, lengths, i, j):
     return length
 
 # Get the objective value of the permutation
-def permutation2objective(p_values, costs, lengths, pairs):
+def permutation2objective(p_values, instance):
+    pairs, costs, lengths = instance.pairs, instance.costs, instance.lengths
     objective = 0
     for (x,y) in pairs:
         objective += costs[get_index(x,y,len(lengths))]*getLength(p_values, lengths, x, y)
     return objective
+
+def getRank(Y, dim):
+    d = [0.0]*dim
+    mosek.LinAlg.syeig(mosek.uplo.lo, dim, Y.level(), d)
+    return sum([d[i] > 1e-6 for i in range(dim)])
+
+def branch_and_bound(instance, M):
+    optimal = False
+    integer = False
+
+    lb = 9999999999
+    ub = 0
+
+    dim = len(instance.pairs)
+    n = len(instance.lengths)
+    
+    while not optimal:
+        # Solve the relaxation
+        problem_sdp3(instance, M, False)
+        Z = M.getVariable("Z")
+        Y = Z.slice([1,1] , [dim+1, dim+1])
+        lb = M.primalObjValue()
+
+        try:
+            M.solve()
+        except OptimizeError as e:
+            print("Optimization failed. Error: {0}".format(e))
+
+        except SolutionError as e:
+            # The solution with at least the expected status was not available.
+            # We try to diagnoze why.
+            print("Requested solution was not available.")
+            prosta = M.getProblemStatus()
+
+            if prosta == ProblemStatus.DualInfeasible:
+                print("Dual infeasibility certificate found.")
+
+            elif prosta == ProblemStatus.PrimalInfeasible:
+                print("Primal infeasibility certificate found.")
+                
+            elif prosta == ProblemStatus.Unknown:
+            # The solutions status is unknown. The termination code
+            # indicates why the optimizer terminated prematurely.
+                print("The solution status is unknown.")
+                symname, desc = mosek.Env.getcodedesc(mosek.rescode(int(M.getSolverIntInfo("optimizeResponse"))))
+                print("   Termination code: {0} {1}".format(symname, desc))
+
+            else:
+                print("Another unexpected problem status {0} is obtained.".format(prosta))
+
+        except Exception as e:
+            print("Unexpected error: {0}".format(e))
+
+        rank = getRank(Y, dim)
+        ub = permutation2objective(solution2permutation(Y.level()[0:dim], n), instance)
+
+        print (rank, lb, ub)
+
+        optimal = True
+        # Check if the solution is integer
+
+        # If not, branch
+        # If yes, check if it is better than the best solution
+        # If yes, update the best solution
+        # If no, prune the branch
+    
+
+
+
+
 
 def main(argv):
 
@@ -353,7 +580,17 @@ def main(argv):
 
     #problem, X = define_problem2(instance)
 
-    problem_mosek(instance, False)  
+    #problem_mosek(instance, False)  
+
+    try:
+        M = Model("Test")    
+        
+        #problem_sdp3(instance, M, False)
+        branch_and_bound(instance, M)
+    finally:
+        M.dispose()
+    
+
 
     #p_values = solution2permutation(X, len(instance.lengths))
 
@@ -365,7 +602,5 @@ def main(argv):
     #print(X.value())
     #print(M.primalObjValue())
     
-
-
 if __name__ == "__main__":
     main(sys.argv[1:])
